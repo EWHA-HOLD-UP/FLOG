a6952d2d35b69a87d66f349c16b012999ad76c26
mingyo choi <128064550+mingyo-choi@users.noreply.github.com>
2023-07-29 23:50:29 +0900

    memorybox_screen 화면 배치 구현중

--- lib/screen/memorybox_screen.dart
+++ lib/screen/memorybox_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
+
 class Memorybox_screen extends StatelessWidget {
   const Memorybox_screen({Key? key}) : super(key: key);
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -10,16 +10,31 @@ class Memorybox_screen extends StatelessWidget {
         title: Text('추억 보관함', style: TextStyle(color: Colors.black),),
         actions: <Widget> [
           IconButton(
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.black,
-              ),
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
             onPressed: () {},
           ),
         ],
         centerTitle: true,
       ),
-      body: Row(
+      body:  Column(
+        children: <Widget> [
+          _buildTop(), //상단
+          _buildMiddle_1(), //중단
+          _buildMiddle_2(),
+          _buildBottom(), //하단
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildTop() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget> [
           Column(
@@ -27,8 +42,10 @@ class Memorybox_screen extends StatelessWidget {
               Icon(
                 Icons.account_circle,
                 size: 60,
+                color: Colors.grey,
               ),
-              Text('예원'),
+              Text('예원', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('eye_o-o')
             ],
           ),
           Column(
@@ -36,8 +53,10 @@ class Memorybox_screen extends StatelessWidget {
               Icon(
                 Icons.account_circle,
                 size: 60,
+                color: Colors.grey,
               ),
-              Text('민교'),
+              Text('민교', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('mingyo')
             ],
           ),
           Column(
@@ -45,12 +64,62 @@ class Memorybox_screen extends StatelessWidget {
               Icon(
                 Icons.account_circle,
                 size: 60,
+                color: Colors.grey,
               ),
-              Text('현서'),
+              Text('현서', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('tocputer')
             ],
           ),
         ],
-      )
+      ),
     );
   }
+
+  Widget _buildMiddle_1() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child : Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.monetization_on,
+            size: 20,
+            color: Colors.green,
+          ),
+          SizedBox(width: 10),
+          Text('모은 개구리 수 : 29마리', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold))
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMiddle_2() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Text('우리 가족의 모든 날',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.left,
+          ),
+          SizedBox(height: 10),
+          Container(
+            height: 150,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10.0)
+              ),
+            )
+          ],
+        ),
+    );
+  }
+
+  Widget _buildBottom() {
+    return Text('하단');
+  }
+
 }
+
+
