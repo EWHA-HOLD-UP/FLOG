6988ff97c3e859bd749f32b66b54decbbda87f48
bboyseo <96673952+bboyseo@users.noreply.github.com>
2023-07-31 04:37:16 +0900

    qpuzzle_screen.dart 상단 로고 추가

--- lib/screen/qpuzzle_screen.dart
+++ lib/screen/qpuzzle_screen.dart
@@ -4,9 +4,33 @@ class Qpuzzle_screen extends StatelessWidget {
   const Qpuzzle_screen({Key? key}) : super(key: key);
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('q-puzzle screen'),
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 30),
+                Image.asset(
+                  "assets/flog_logo.png",
+                  width: 55,
+                  height: 55,
+                ),
+                Text(
+                  "Q-puzzle",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF609966), // #609966 색상 지정
+                  ),
+                ),
+                SizedBox(height: 20), // 간격
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
