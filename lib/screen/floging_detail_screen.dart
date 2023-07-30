950484b0b6575885883c3ff1ad87441ccc23873e
bboyseo <96673952+bboyseo@users.noreply.github.com>
2023-07-31 04:33:10 +0900

    floging_screen.dart 와 floging_detail_screen.dart 배치 구현
    floging_screen.dart 과 floging_detail_screen.dart 뼈대 설계 완료
    floging_screen 안의 상태 박스 클릭시 floging_detail_screen.dart로 이동하고, 뒤로가기를 통해 돌아오기 가능

---
+++ lib/screen/floging_detail_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Floging_Detail_Screen extends StatelessWidget {
+  const Floging_Detail_Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    // 뒤로가기 버튼 클릭 시 이전 페이지(Floging_Screen)로 이동
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+
+            Image.asset(
+              "assets/flog_logo.png",
+              width: 55,
+              height: 55,
+            ),
+            Text(
+              "FLOGing",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+                color: Color(0xFF609966),
+              ),
+            ),
+            SizedBox(height: 20), // 간격
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/floging_screen.dart
+++ lib/screen/floging_screen.dart
@@ -1,13 +1,79 @@
-//floging screen
+
 import 'package:flutter/material.dart';
+import 'floging_detail_screen.dart'; // 다른 파일의 Floging_Detail_Screen import
 
 class Floging_Screen extends StatelessWidget {
   const Floging_Screen({Key? key}) : super(key: key);
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('floging screen'),
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height:30),
+                  Image.asset(
+                    "assets/flog_logo.png",
+                    width: 55,
+                    height: 55,
+                  ),
+                  Text(
+                    "FLOGing",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF609966), // #609966 색상 지정
+                    ),
+                  ),
+              SizedBox(height:20), // 간격
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 20.0,
+                    mainAxisSpacing: 20.0,
+                    childAspectRatio: 3 / 4,
+                  ),
+                  itemCount: 3, // 박스의 개수
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                    onTap: () { // 클릭 시 다른 스크린으로 이동하는 로직 추가
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Floging_Detail_Screen(),
+                        ),
+                      );
+                    },
+                      child: Container(
+                      width: 150,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          color: Color(0xad747474),
+                        borderRadius: BorderRadius.circular(23), // 모서리 둥글기 조절
+                      ),
+                      child: Center(
+                        child: Text(
+                          "플로깅 상태 ${index + 1}",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    );
+                  },
+                ),
+              ),
+              ),
+                ],
+              ),
+            )
+
+        ),
       ),
     );
   }
