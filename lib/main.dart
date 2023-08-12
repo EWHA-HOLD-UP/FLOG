2681fc9676749638bd0bf561db6d47764a63caec
bboyseo <96673952+bboyseo@users.noreply.github.com>
2023-08-12 19:20:49 +0900

    메인다트 복구

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flog/screen/floging/shooting_screen_back.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flog/screen/register/start_login_screen.dart';
+import 'package:flog/screen/register/personal_nickname_screen.dart';
+import 'package:flog/screen/register/sms_login_home.dart';
 import 'firebase_options.dart';
-import 'package:flog/screen/root_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //Firebase 초기화 코드
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RootScreen(matched_familycode: 'ohyeah',),
+      title: 'Flog',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/init',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/login': (context) => const NicknameScreen(),
+        '/init' : (context) => const ShootingScreen(),
+      },
     );
   }
 }
