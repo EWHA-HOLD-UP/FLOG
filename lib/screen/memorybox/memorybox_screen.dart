ee4f54f1e049a50f7f7abb92cdcf0c0a592c65ab
mingyo choi <128064550+mingyo-choi@users.noreply.github.com>
2023-08-28 00:22:52 +0900

    MemoryBox_Screen 하단부까지 구현완료

--- lib/screen/memorybox/memorybox_screen.dart
+++ lib/screen/memorybox/memorybox_screen.dart
@@ -42,8 +42,8 @@ class MemoryBoxState extends State<MemoryBoxScreen> {
         children: <Widget> [
           memberProfiles(numOfMem), //가족 구성원들의 프로필 보여주기
           flogCoinNum(), //모은 개구리 수 보여주기
-          _buildMiddle_2(),
-          _buildBottom(), //하단
+          ourEveryday(),
+          ourValuableday(), //하단
         ],
       ),
     );
@@ -55,8 +55,8 @@ class MemoryBoxState extends State<MemoryBoxScreen> {
 
     /* 나중에는 아래 people.add 5줄 다 지우고,
 
-    getProfileNums{여기에 파이어베이스에서 숫자 받아오는 함수 구현}
-    getNicknames{여기에 파이어베이스에서 닉네임 받아오는 함수 구현}
+    getProfileNums{여기에 파이어베이스에서 순서대로 숫자:프사 인덱스 받아오는 함수 구현}
+    getNicknames{여기에 파이어베이스에서 순서대로 닉네임 받아오는 함수 구현}
     for(int i = 1; i <= numOfMem; i++) {
       int 숫자 = getProfileNums();
       String 닉네임 = getNicknames();
@@ -110,35 +110,35 @@ class MemoryBoxState extends State<MemoryBoxScreen> {
     );
   }
 
-  Widget _buildMiddle_2() {
+  Widget ourEveryday() {
     int today = 7; //오늘 날짜
     return Padding(
       padding: const EdgeInsets.only(top: 20),
       child: Column(
-        children: [
-          const Text('우리 가족의 모든 날',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.left,
-          ),
-          const SizedBox(height: 10),
-          Container(
-            height: 200,
-            width: 350,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(10.0),
+          children: [
+            const Text('우리 가족의 모든 날',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.left,
             ),
-            child: Column(
-              children: [
-                const SizedBox(height: 15),
-                Flexible(
-                  child: GridView.builder(
+            const SizedBox(height: 10),
+            Container(
+              height: 200,
+              width: 350,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(height: 15),
+                  Flexible(
+                    child: GridView.builder(
                       padding: const EdgeInsets.all(8.0), // 각 컨테이너 사이의 간격 설정
                       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 7, //열 수
                         mainAxisSpacing: 10.0, // 행 사이의 간격
                       ),
-                      itemCount: 14, //전체 사진 수
+                      itemCount: 31, //전체 날짜 수 - 나중에 월별로 수정?
                       itemBuilder: (BuildContext context, int index) {
                         //각 그리드 아이템에 표시할 위젯을 반환
                         final containerNumber = index + 1;
@@ -177,9 +177,9 @@ class MemoryBoxState extends State<MemoryBoxScreen> {
                                   ),
                                 ),
                                 Center(
-                                    child: Text(containerNumber.toString(),
-                                      style: const TextStyle(color: Colors.white),
-                                    ),
+                                  child: Text(containerNumber.toString(),
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
                                 ),
                               ],
                             ),
@@ -187,14 +187,115 @@ class MemoryBoxState extends State<MemoryBoxScreen> {
                         }
                       },
                     ),
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(width: 133),
+                      OutlinedButton(
+                        onPressed: (){
+                          //'전체보기' 클릭 시 나타나는 화면 제작 후 구현 필요
+                        },
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(color: Colors.white),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: const Text(
+                          '전체 보기',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 80),
+                      InkWell(
+                        onTap: () {
+                          //자동 영상 생성 구현 필요
+                        },
+                        child: Image.asset( //전송 버튼
+                            "button/video.png",
+                            width: 35,
+                            height: 35
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15)
+                ],
+              ),
+            ),
+          ]
+      ),
+    );
+  }
+
+  Widget ourValuableday() {
+    int solvedPuzzle = 8; //다 푼 퍼즐 수 (임의 설정) - 나중에 파이어베이스에서 불러오기
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          const Text('우리 가족의 소중한 날',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.left,
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 250,
+            width: 350,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                Expanded(
+                  child: GridView.builder(
+                    padding: const EdgeInsets.all(8.0), // 각 컨테이너 사이의 간격 설정
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      childAspectRatio: 2.0 / 3.0, //가로:세로 2:3 비율
+                      crossAxisSpacing: 10.0,
+                      mainAxisSpacing: 10.0, // 행 사이의 간격
+                    ),
+                    itemCount: solvedPuzzle, //전체 사진 수
+                    itemBuilder: (BuildContext context, int index) {
+                      int reversedIndex = solvedPuzzle - index; //역순으로 퍼즐 사진 인덱스 계산 : 가장 최근에 푼 것부터 보여주려고
+                      String imagePath = "assets/emoticons/emoticon_$reversedIndex.png"; //나중에 파이어베이스에서 받아오는 걸로 경로 수정
+
+                      //각 그리드 아이템에 표시할 위젯을 반환
+                      return Container(
+                        margin: const EdgeInsets.all(3.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                              color: Colors.white,
+                              width: 1.5
+                          ),
+                          color: const Color(0xFFCED3CE),
+                        ),
+                        child: Center(
+                          child: ClipRRect(
+                            child: Image.asset(
+                              imagePath,
+                              alignment: Alignment.center,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
                 Row(
                   children: [
                     const SizedBox(width: 133),
                     OutlinedButton(
                       onPressed: (){
-
-                        },
+                        //'전체보기' 클릭 시 나타나는 화면 제작 후 구현 필요
+                      },
                       style: OutlinedButton.styleFrom(
                         side: const BorderSide(color: Colors.white),
                         shape: RoundedRectangleBorder(
@@ -212,10 +313,10 @@ class MemoryBoxState extends State<MemoryBoxScreen> {
                     const SizedBox(width: 80),
                     InkWell(
                       onTap: () {
-
-                        },
+                        //추억북 신청 화면으로 넘어가기 구현 필요
+                      },
                       child: Image.asset( //전송 버튼
-                          "button/video.png",
+                          "button/book.png",
                           width: 35,
                           height: 35
                       ),
@@ -226,83 +327,11 @@ class MemoryBoxState extends State<MemoryBoxScreen> {
               ],
             ),
           ),
-        ]
-      ),
-    );
-  }
-
-  Widget _buildBottom() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20),
-      child: Column(
-        children: [
-          const Text('우리 가족의 소중한 날',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.left,
-          ),
-          const SizedBox(height: 10),
-          Container(
-            height: 200,
-            width: 350,
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10.0)
-            ),
-            child: Column(
-              children: [
-                const SizedBox(height: 10),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.photo,
-                      size: 120,
-                      color: Colors.white,
-                    ),
-                    Icon(
-                      Icons.photo,
-                      size: 120,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    const SizedBox(width: 125),
-                    OutlinedButton(
-                      onPressed: (){
-
-                      },
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(color: Colors.white),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                      child: const Text(
-                        '전체 보기',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    const SizedBox(width: 80),
-                    IconButton(
-                      onPressed: (){
-
-                      },
-                      icon: const Icon(
-                        Icons.menu_book,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+          const SizedBox(height: 15)
         ],
       ),
     );
   }
+
 
 }
