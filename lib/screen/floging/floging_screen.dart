b93a77620c58b171d78cbfd682bed54478601551
bboyseo <96673952+bboyseo@users.noreply.github.com>
2023-10-04 03:19:58 +0900

    [memoryBox] 모든날 전체보기 구현 완료

--- lib/screen/floging/floging_screen.dart
+++ lib/screen/floging/floging_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flog/screen/floging/floging_detail_screen.dart';
--- lib/screen/memorybox/memorybox_everyday_showall_screen.dart
+++ lib/screen/memorybox/memorybox_everyday_showall_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
-
+import 'dart:math';
 import 'memorybox_detail_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MemoryBoxEverydayShowAllScreen extends StatelessWidget {
   const MemoryBoxEverydayShowAllScreen({Key? key}) : super(key: key);
@@ -61,18 +63,36 @@ class MemoryBoxInfiniteCalendar extends StatefulWidget {
 class MemoryBoxInfiniteCalendarState extends State<MemoryBoxInfiniteCalendar> {
   late PageController _pageController;
   late DateTime _currentDate;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final currentUser = FirebaseAuth.instance.currentUser!;
+  String currentUserFlogCode = ""; // 현재 로그인한 사용자의 flogCode
 
   @override
   void initState() {
     super.initState();
     _currentDate = DateTime.now();
     _pageController = PageController(initialPage: 100); //100개월(=8년 4개월정도..)까지 저장 가능
+    getUserData();
   }
 
   void _onPageChanged(int page) {
     setState(() {
       _currentDate = DateTime.now().add(Duration(days: (page - 99) * 30));
     });
+  }
+
+  Future<void> getUserData() async {
+    final userDoc = await FirebaseFirestore.instance
+        .collection('User')
+        .doc(currentUser.email)
+        .get();
+
+    if (userDoc.exists) {
+      setState(() {
+        currentUserFlogCode = userDoc.data()!['flogCode'];
+      });
+    }
+    print(currentUserFlogCode);
   }
 
   @override
@@ -238,65 +258,193 @@ class MemoryBoxInfiniteCalendarState extends State<MemoryBoxInfiniteCalendar> {
               onTap: () {},
               child: Container(
                 margin: const EdgeInsets.all(3.0),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFCED3CE),
-                  borderRadius: BorderRadius.circular(15),
-                ),
                 alignment: Alignment.center,
                 child: Text(
                   containerNumber.toString(),
                   style: GoogleFonts.inter(
                     textStyle: const TextStyle(
-                      color: Colors.white,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ),
             );
           } else {
-            //현재 월 이전 또는 현재 월의 오늘 이전인 경우 플로깅 이미지로 채워진 컨테이너
-            return GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        MemoryBoxDetailScreen(
+            return StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('Floging')
+                  .where('date', isGreaterThanOrEqualTo: Timestamp.fromDate(DateTime(year, month, containerNumber)))
+                  .where('date', isLessThan: Timestamp.fromDate(DateTime(year, month, containerNumber + 1)))
+                  .where('flogCode', isEqualTo: currentUserFlogCode)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  debugPrint('Error: ${snapshot.error}');
+                  return Text('Error: ${snapshot.error}');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+
+                final flogDocuments = snapshot.data?.docs ?? [];
+
+                if (flogDocuments.isEmpty) {
+                  // 플로깅 데이터가 없을 때 회색 동그라미를 반환
+                  return Container(
+                    margin: const EdgeInsets.all(3.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      containerNumber.toString(),
+                      style: GoogleFonts.inter(
+                        textStyle: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+
+                // 가져온 플로깅 사진을 사용하여 이미지 컨테이너를 생성하고 반환
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => MemoryBoxDetailScreen(
                           selectedDate: formattedDate,
                         ),
+                      ),
+                    );
+                  },
+                  child: Stack(
+                    children: [
+                      ..._buildFlogImageContainers(flogDocuments), // 플로깅 사진을 추가
+                      Center(
+                        child: Text(
+                          containerNumber.toString(),
+                          style: const TextStyle(color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
                   ),
                 );
               },
-              child: Container(
-                margin: const EdgeInsets.all(3.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: const Color(0xFFCED3CE),
-                ),
-                child: Stack(
-                  children: [
-                    Center(
-                      child: ClipRRect(
-                        child: Image.asset(
-                          "assets/emoticons/emoticon_$containerNumber.png",
-                          width: 35,
-                          height: 35,
-                          alignment: Alignment.center,
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: Text(
-                        containerNumber.toString(),
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
             );
           }
         },
       ),
     );
   }
+
+  List<Widget> _buildFlogImageContainers(List<DocumentSnapshot> flogDocuments) {
+    List<Widget> imageContainers = [];
+
+    double radius = 20 * 0.5;
+
+    if (flogDocuments.length == 1) {
+      // flogDocuments의 길이가 1인 경우, 동그라미를 센터에 놓음
+      final flogData = flogDocuments[0].data() as Map<String, dynamic>;
+      final backImageURL = flogData['downloadUrl_back'];
+
+      imageContainers.add(
+        Center(
+          child: Container(
+            width: 20,
+            height: 20,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color(0xffd6d6d6),
+              image: DecorationImage(
+                image: NetworkImage(backImageURL),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      );
+    } else if (flogDocuments.length == 2) {
+      // flogDocuments의 길이가 2인 경우, 대각선으로 배치
+      final flogData1 = flogDocuments[0].data() as Map<String, dynamic>;
+      final flogData2 = flogDocuments[1].data() as Map<String, dynamic>;
+
+      final backImageURL1 = flogData1['downloadUrl_back'];
+      final backImageURL2 = flogData2['downloadUrl_back'];
+
+      imageContainers.add(
+        Positioned(
+          left: 8, // 왼쪽 상단에 배치
+          top: 8,
+          child: Container(
+            width: 20,
+            height: 20,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color(0xffd6d6d6),
+              image: DecorationImage(
+                image: NetworkImage(backImageURL1),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      );
+
+      imageContainers.add(
+        Positioned(
+          right: 8, // 오른쪽 하단에 배치
+          bottom: 8,
+          child: Container(
+            width: 20,
+            height: 20,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color(0xffd6d6d6),
+              image: DecorationImage(
+                image: NetworkImage(backImageURL2),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      );
+    } else {
+      // flogDocuments의 길이가 3 이상인 경우, 세잎 클로버 모양으로 배치
+      for (int i = 0; i < flogDocuments.length; i++) {
+        double angle = (-pi / 2) + (2 * pi / flogDocuments.length) * i; // 정삼각형의 각도 계산
+
+        double centerX = radius * cos(angle) + radius; // 중점 x 좌표 계산
+        double centerY = radius * sin(angle) + radius; // 중점 y 좌표 계산
+
+        final flogData = flogDocuments[i].data() as Map<String, dynamic>;
+        final backImageURL = flogData['downloadUrl_back'];
+
+        imageContainers.add(
+          Positioned(
+            left: centerX + 5, // 이미지 컨테이너 중점 기준으로 x 좌표 조절
+            top: centerY + 5, // 이미지 컨테이너 중점 기준으로 y 좌표 조절
+            child: Container(
+              width: 20,
+              height: 20,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color(0xffd6d6d6),
+                image: DecorationImage(
+                  image: NetworkImage(backImageURL),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+    }
+    return imageContainers;
+  }
 }
+
