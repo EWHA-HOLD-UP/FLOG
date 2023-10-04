53336834397e6243f37bc73965b6c1125804ab8f
bboyseo <96673952+bboyseo@users.noreply.github.com>
2023-10-04 17:57:48 +0900

    [memoryBox] 소중한날 전체보기 : 사진 불러오기

--- lib/screen/memorybox/memorybox_valuableday_showall_screen.dart
+++ lib/screen/memorybox/memorybox_valuableday_showall_screen.dart
@@ -1,91 +1,176 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'memorybox_detail_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class MemoryBoxValuabledayShowAllScreen extends StatelessWidget {
+
+class MemoryBoxValuabledayShowAllScreen extends StatefulWidget {
   const MemoryBoxValuabledayShowAllScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    int solvedPuzzle = 8; //다 푼 퍼즐 수 (임의 설정) - 나중에 파이어베이스에서 불러오기
+  _MemoryBoxValuabledayShowAllScreenState createState() =>
+      _MemoryBoxValuabledayShowAllScreenState();
+}
 
+class _MemoryBoxValuabledayShowAllScreenState
+    extends State<MemoryBoxValuabledayShowAllScreen> {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final currentUser = FirebaseAuth.instance.currentUser!;
+  String currentUserFlogCode = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getUserFlogCode();
+  }
+
+  //현재 로그인한 사용자의 flogCode를 Firestore에서 가져오는 함수
+  Future<void> getUserFlogCode() async {
+    final userDoc = await FirebaseFirestore.instance
+        .collection('User')
+        .doc(currentUser.email)
+        .get();
+
+    if (userDoc.exists) {
+      setState(() {
+        currentUserFlogCode = userDoc.data()!['flogCode'];
+      });
+    }
+    print(currentUserFlogCode);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           icon: const Icon(
             Icons.arrow_back,
-            color: Colors.black, // 뒤로가기 버튼 아이콘 색상
-          ), // 이미지 경로 지정
+            color: Colors.black,
+          ),
           onPressed: () {
-            Navigator.pop(context); // 뒤로가기 기능 추가
+            Navigator.pop(context);
           },
         ),
         backgroundColor: Colors.white,
         title: Row(
           children: [
             SizedBox(width: 95),
-            Text('소중한 날',
+            Text(
+              '소중한 날',
               style: GoogleFonts.inter(
-                  textStyle: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  )),
+                textStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
-        elevation: 0.0, //그림자 없음
+        elevation: 0.0,
         centerTitle: true,
       ),
       body: Center(
         child: SafeArea(
           child: Column(
-              children: [
-                SizedBox(height: 25), //간격
-                Expanded(
-                    child: GridView.builder(
-                      padding: const EdgeInsets.all(8.0), // 각 컨테이너 사이의 간격 설정
-                      gridDelegate:
-                      const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: 2.0 / 3.0, //가로:세로 2:3 비율
-                        crossAxisSpacing: 10.0,
-                        mainAxisSpacing: 10.0, // 행 사이의 간격
-                      ),
-                      itemCount: solvedPuzzle, //전체 사진 수
-                      itemBuilder: (BuildContext context, int index) {
-                        int reversedIndex = solvedPuzzle -
-                            index; //역순으로 퍼즐 사진 인덱스 계산 : 가장 최근에 푼 것부터 보여주려고
-                        String imagePath =
-                            "assets/emoticons/emoticon_$reversedIndex.png"; //나중에 파이어베이스에서 받아오는 걸로 경로 수정
+            children: [
+              SizedBox(height: 25),
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('Qpuzzle')
+                      .where('flogCode', isEqualTo: currentUserFlogCode)
+                      .where('isComplete', isEqualTo: true)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    }
 
-                        //각 그리드 아이템에 표시할 위젯을 반환
-                        return Container(
-                          margin: const EdgeInsets.all(3.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            border: Border.all(color: Colors.white, width: 1.5),
-                            color: const Color(0xFFCED3CE),
-                          ),
-                          child: Center(
-                            child: ClipRRect(
-                              child: Image.asset(
-                                imagePath,
-                                alignment: Alignment.center,
-                              ),
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    }
+
+                    final docs = snapshot.data?.docs ?? [];
+
+                    if (docs.isEmpty) {
+                      return Center(
+                        child: Text(
+                          '완성된 소중한 날이 아직 없어요.\n\n어서 큐퍼즐을 완성해보세요!',
+                          style: GoogleFonts.nanumGothic(
+                            textStyle: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black38,
                             ),
                           ),
+                          textAlign: TextAlign.center,
+                        ),
+                      );
+                    }
+
+                    return GridView.builder(
+                      padding: const EdgeInsets.all(8.0),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 2.0 / 3.0,
+                        crossAxisSpacing: 10.0,
+                        mainAxisSpacing: 10.0,
+                      ),
+                      itemCount: docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final imagePath = docs[index]['pictureUrl'];
+                        final puzzlenumber = docs[index]['puzzleNo'];
+
+                        return Stack(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.all(3.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                border: Border.all(color: Colors.white, width: 1.5),
+                                color: const Color(0xFFCED3CE),
+                                image: DecorationImage(
+                                  image: NetworkImage(imagePath),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 10,
+                              left: 10,
+                              child: Container(
+                                padding: const EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                  color: Color(0x99ffffff),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Text(
+                                  '#$puzzlenumber',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         );
                       },
-                    ),
-                )
+                    );
 
-
-              ]
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
 
