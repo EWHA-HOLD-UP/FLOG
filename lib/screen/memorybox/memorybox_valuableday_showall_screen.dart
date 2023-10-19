686322248fc41bed7fb7173c2a42367ee75625ff
bboyseo <96673952+bboyseo@users.noreply.github.com>
2023-10-19 13:33:09 +0900

    [memoryBox] 소중한날 질문별 답변 불러오기 구현 완료

--- lib/screen/memorybox/memorybox_valuableday_showall_screen.dart
+++ lib/screen/memorybox/memorybox_valuableday_showall_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'memorybox_detail_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -55,6 +54,217 @@ class _MemoryBoxValuabledayShowAllScreenState
     }
 
     return questions;
+  }
+
+  void _showQuestionBottomSheet(int puzzleNumber, int questionNo) async {
+
+    questionNo = questionNo -1;
+    showModalBottomSheet(
+        context: context,
+        backgroundColor: Colors.white,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+        ),
+        builder: (BuildContext context) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.7,
+            child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('User')
+                    .where('flogCode', isEqualTo: currentUserFlogCode)
+                    .snapshots(),
+                builder: (context, userSnapshot) {
+                  if (userSnapshot.hasError) {
+                    return Text('Error: ${userSnapshot.error}');
+                  }
+                  if (userSnapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+
+                  final userDocuments = userSnapshot.data!.docs;
+                  userDocuments.sort((a, b) {
+                    final aData = a.data() as Map<String, dynamic>;
+                    final bData = b.data() as Map<String, dynamic>;
+                    final aEmail = aData['email'] as String;
+                    final bEmail = bData['email'] as String;
+
+                    if (aEmail == currentUser.email) {
+                      return -1;
+                    } else if (bEmail == currentUser.email) {
+                      return 1;
+                    } else {
+                      return aEmail.compareTo(bEmail); //나머지 알파벳순 정렬
+                    }
+                  });
+
+                  return ListView(
+                    children: [
+                      const SizedBox(height: 25),
+                      Center(
+                        child: Image.asset(
+                          "assets/flog_logo.png",
+                          width: 70,
+                          height: 70,
+                          alignment: Alignment.centerLeft,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      StreamBuilder <QuerySnapshot> (
+                          stream: FirebaseFirestore.instance
+                              .collection("Question")
+                              .where('puzzleNo', isEqualTo: puzzleNumber)
+                              .where('questionNo', isEqualTo: questionNo)
+                              .snapshots(),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            }
+                            if (!snapshot.hasData) {
+                              return CircularProgressIndicator();
+                            }
+
+                            final questionData = snapshot.data!.docs.isNotEmpty
+                                ? snapshot.data!.docs.first.data() as Map<String, dynamic>
+                                : null;
+                            if (questionData == null) {
+                              return Text('Question not found');
+                            }
+                            final questionContent = questionData['questionContent']; // 질문 내용 가져오기
+                            return Center(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20), //왼쪽과 오른쪽 간격 지정
+                                child: Text(
+                                  'Q${questionNo + 1}. $questionContent',
+                                  style: const TextStyle(
+                                      fontSize: 22, fontWeight: FontWeight.bold
+                                  ),
+                                  textAlign: TextAlign.left,
+                                  softWrap: true, //자동 줄바꿈
+                                ),
+                              ),
+                            );
+                          }
+                      ),
+                      const SizedBox(height: 25),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: userDocuments.length,
+                        itemBuilder: (context, rowIndex) {
+                          final userData = userDocuments[rowIndex].data() as Map<String, dynamic>;
+                          final userProfile = userData['profile'];
+                          final userNickname = userData['nickname'];
+                          //각 사용자에 대한 답변을 불러오기
+                          final answerCollection = FirebaseFirestore.instance.collection('Answer');
+                          final query = answerCollection
+                              .where('flogCode', isEqualTo: currentUserFlogCode)
+                              .where('puzzleNo', isEqualTo: puzzleNumber)
+                              .where('questionNo', isEqualTo: questionNo);
+
+                          return StreamBuilder <QuerySnapshot> (
+                              stream: query.snapshots(),
+                              builder: (context, answerSnapshot){
+                                if(answerSnapshot.hasError){
+                                  return Text('Error: ${answerSnapshot.error}');
+                                }
+                                if (answerSnapshot.connectionState == ConnectionState.waiting) {
+                                  return CircularProgressIndicator();
+                                }
+                                final answerDocuments = answerSnapshot.data!.docs;
+                                String userAnswer = '';
+                                  for (final answerDoc in answerDocuments) {
+                                    final answers = answerDoc['answers'] as Map<String, dynamic>;
+                                    final userAnswerText = answers[userData['email']];
+                                    if(userAnswerText != null) {
+                                      userAnswer = userAnswerText;
+                                      break;
+                                    }
+                                  }
+
+                                return Container(
+                                      width: double.infinity,
+                                      height: 110,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.transparent,
+                                      ),
+                                      margin: const EdgeInsets.symmetric(
+                                          vertical: 10, horizontal: 20),
+                                      child: Row(
+                                        children: [
+                                          const SizedBox(width: 15),
+                                          Column(
+                                            mainAxisAlignment: MainAxisAlignment.start,
+                                            children: [
+                                              const SizedBox(height: 10),
+                                              Hero(
+                                                tag: "profile",
+                                                child: Stack(
+                                                  children: [
+                                                    Container(
+                                                      width: 50,
+                                                      height: 50,
+                                                      decoration: BoxDecoration(
+                                                        shape: BoxShape.circle,
+                                                        color: Colors.grey[200],
+                                                      ),
+                                                      child: Center(
+                                                        child: ClipOval(
+                                                          child: Image.asset(
+                                                            "assets/profile/profile_${userProfile}.png",
+                                                            width: 40,
+                                                            height: 40,
+                                                            alignment: Alignment.center,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(width: 20),
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                userNickname,
+                                                style: GoogleFonts.nanumGothic(
+                                                  textStyle: TextStyle(
+                                                    fontSize: 17,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                              ),
+                                              SizedBox(height:7),
+                                              Container(
+                                                width: 290,
+                                                child: Text(
+                                                userAnswer,
+                                                style: const TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.black,
+                                                ),
+                                                softWrap: true, // 자동 줄바꿈
+                                              ),
+                                              )
+                                            ],
+                                          )
+                                        ],
+                                      )
+                                );
+                              });
+                        },
+                      ),
+                    ],
+                  );
+                }
+            ),
+          );
+        });
   }
 
   @override
@@ -201,16 +411,22 @@ class _MemoryBoxValuabledayShowAllScreenState
                                             if (entry.key > 0) Divider(height: 1, thickness: 1, color: Color(0xff609966)), // 상단 구분선 (첫 번째 질문 이후부터 추가)
                                             Padding(
                                               padding: const EdgeInsets.symmetric(vertical: 8.0), // 상단과 하단 간격 조절
-                                              child: Text(
-                                                '#$questionNo. $question',
-                                                style: GoogleFonts.nanumGothic(
-                                                  textStyle: TextStyle(
-                                                    fontSize: 12,
-                                                    color: Colors.black,
-                                                    fontWeight: FontWeight.bold,
+                                              child: GestureDetector(
+                                                onTap: () {
+                                                  _showQuestionBottomSheet(puzzleNumber, questionNo);
+                                                },
+                                                child: Text(
+                                                  '#$questionNo. $question',
+                                                  style: GoogleFonts.nanumGothic(
+                                                    textStyle: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.black,
+                                                      fontWeight: FontWeight.bold,
+                                                    ),
                                                   ),
                                                 ),
                                               ),
+
                                             ),
                                           ],
                                         );
